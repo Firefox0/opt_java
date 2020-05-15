57d1f28955154bcade8c1d498441dba8b55f7257
Firefox0 <>
2020-05-15 17:05:17 +0200

    added inputs in main

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    
     public static void main(String[] args) {
+        System.out.println("-- OPT Downloader --\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Starting episode: ");
+        int start = scanner.nextInt();
+        System.out.print("Ending episode (including): ");
+        int end = scanner.nextInt();
+        System.out.print("Amount threads: ");
+        int amount_threads = scanner.nextInt();
+        scanner.close();
+        System.out.print("\n");
         Requests r = new Requests();
-        r.main(1, 5);
+        r.main(start, end);
+        System.out.print("\n");
         Multithreading mt = new Multithreading(r);
-        mt.threads(3);
+        mt.threads(amount_threads);
     }
+
 }
