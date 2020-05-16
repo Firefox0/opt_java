7eb9ef916557aa05c4902f34b5e6ae6c6d2ab824
Firefox0 <>
2020-05-16 15:56:15 +0200

    seperate class for pair

---
+++ Pair.java
@@ -0,0 +1,17 @@
+public class Pair {
+    private int integer;
+    private String string;
+
+    public Pair(Integer integer, String string) {
+        this.integer = integer;
+        this.string = string;
+    }
+
+    public int integer() {
+        return this.integer;
+    }
+
+    public String string() {
+        return string;
+    }
+}
